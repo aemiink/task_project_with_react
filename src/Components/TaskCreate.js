53e4a9376e6f02e9a684fe42f388bd862144f257
Emin <102097443+aemiink@users.noreply.github.com>
2024-07-17 17:33:51 +0300

    Update TaskCreate.js

---
+++ src/Components/TaskCreate.js
@@ -0,0 +1,79 @@
+import './ComCss/taskCreate.css'
+import 'bulma/css/bulma.css'
+import {useState} from "react";
+
+function TaskCreate({onCreate, task, taskformUpdate, onUpdate}){
+    const [title, setTitle] = useState(task ? task.title : "")
+    const [description, setDescription] = useState(task ? task.description : "")
+
+
+    const handleChange = (event) => {
+        setTitle(event.target.value);
+    }
+
+    const handleDescChange = (event) => {
+        setDescription(event.target.value);
+    }
+
+    const submitForm = (event) => {
+        event.preventDefault();
+        if(taskformUpdate) {
+            onUpdate(task.id, title, description);
+        }
+        else{
+            onCreate(title, description);
+        }
+        setTitle('');
+        setDescription('');
+    }
+    return(
+        <div>{taskformUpdate ? (<div className="App">
+            <form className="forms">
+                <div className="field">
+                    <label className="label">Edit Title</label>
+                    <div className="control">
+                        <input value={title} onChange={handleChange} className="input" type="text"
+                               placeholder="Enter the title"/>
+                    </div>
+                </div>
+                <div className="field">
+                    <label className="label">Edit Tasks</label>
+                    <div className="control">
+                        <textarea value={description} onChange={handleDescChange} rows="6" className="textarea"
+                                  placeholder="Enter the task"></textarea>
+                    </div>
+                </div>
+                <div className="field is-grouped">
+                    <div className="control">
+                        <button onClick={submitForm} className="button is-warning">Edit</button>
+                    </div>
+                </div>
+            </form>
+        </div>):(<div className="App">
+            <form className="forms">
+                <div className="field">
+                    <label className="label">Title</label>
+                    <div className="control">
+                        <input value={title} onChange={handleChange} className="input" type="text"
+                               placeholder="Enter the title"/>
+                    </div>
+                </div>
+                <div className="field">
+                    <label className="label">Add Task!</label>
+                    <div className="control">
+                        <textarea value={description} onChange={handleDescChange} rows="6" className="textarea"
+                                  placeholder="Enter the task"></textarea>
+                    </div>
+                </div>
+                <div className="field is-grouped">
+                    <div className="control">
+                        <button onClick={submitForm} className="button is-primary">Submit</button>
+                    </div>
+                </div>
+            </form>
+        </div>)
+        }</div>
+    );
+}
+
+export default TaskCreate;
