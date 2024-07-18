4d24dde4b3d25c31c9cc16aa41f8a3687563b351
Emin <102097443+aemiink@users.noreply.github.com>
2024-07-18 17:04:14 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,62 +1,23 @@
 import './App.css';
 import TaskCreate from "./Components/TaskCreate";
 import TaskList from "./Components/TaskList";
-import {useEffect, useState} from "react";
-import axios from "axios";
+import {useEffect, useState, useContext} from "react";
+import TasksContexts from "./Context/task";
 
 function App() {
-    //Taskları depolamak için tasklar için yeni bir useState() kullanıyoruz. Bu tüm taskleri depolayacağı için array açıyoruz.
-    const [tasks, setTasks] = useState([])
-
-    const createTask = async (title, description) => {
-
-        const response = await axios.post('http://localhost:3004/tasks', {title, description});
-        const createdTask = [
-            // ... spreate özelliğidir. eski değerleri de etkleyecek.
-            ...tasks,
-            response.data
-        ];
-        // setTasks() metoduylaa create ettik.
-        setTasks(createdTask);
-    };
-    const deleteTaskById = async (id) => {
-        await axios.delete(`http://localhost:3004/tasks/${id}`)
-        const afterDelTask = tasks.filter((task) => {
-            return task.id !== id;
-        });
-        setTasks(afterDelTask);
-    }
-
-    const fetchTask = async () => {
-        const response = await axios.get('http://localhost:3004/tasks')
-        setTasks(response.data);
-    }
-
+    const {fetchTask} = useContext(TasksContexts)
     useEffect(() => {
         fetchTask();
     }, []);
 
-    const editTaskById = async (id, updateTitle, updateDesc) => {
-        await axios.put(`http://localhost:3004/tasks/${id}`, {
-            title: updateTitle,
-            description: updateDesc
-        })
-        const updatingTasks = tasks.map((task) => {
-            if ( task.id === id){
-                return {id , title:updateTitle, description:updateDesc}
-            }
-            return task;
-        });
-        setTasks(updatingTasks);
-    }
-
     return (
     <div className="App">
-      <TaskCreate onCreate={createTask} />
+      <TaskCreate />
       <h1 className="title"> Tasks </h1>
-      <TaskList tasks={tasks} onDelete={deleteTaskById} onUpdate={editTaskById}/>
+      <TaskList />
     </div>
   );
 }
 
 export default App;
+
